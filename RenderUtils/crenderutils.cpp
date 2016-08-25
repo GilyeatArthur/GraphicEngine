bbb0062a432a0a1e8e781136417d676bedd243dc
GilyeatArthur <gilyeatarthur@hotmail.com>
2016-08-25 14:26:52 -0700

    Added Shader function

--- RenderUtils/crenderutils.cpp
+++ RenderUtils/crenderutils.cpp
@@ -4,7 +4,9 @@
 #include "crenderutils.h"
 #include "Vertex.h"
 
-Geometry makeGeometry(struct Vertex *verts, size_t vsize, unsigned int	*tris, size_t tsize)
+#include <cstdio>
+
+Geometry makeGeometry(const struct Vertex *verts, size_t vsize, const unsigned int	*tris, size_t tsize)
 {
 	Geometry retval;
 	//Define the Variables
@@ -44,4 +46,33 @@ void freeGeometry(Geometry &geo)
 	glDeleteBuffers(1, &geo.ibo);
 	glDeleteVertexArrays(1, &geo.vao);
 	geo = { 0,0,0,0 };
+}
+
+Shader makeShader(const char * vsource, const char * fsource)
+{
+	Shader retval;
+	//create our variables
+	retval.handle = glCreateProgram();
+	unsigned vs = glCreateShader(GL_VERTEX_SHADER);
+	unsigned fs = glCreateShader(GL_FRAGMENT_SHADER);
+	//initialize our variables
+	glShaderSource(vs, 1, &vsource, 0);
+	glShaderSource(fs, 1, &fsource, 0);
+	//compile the shaders
+	glCompileShader(vs);
+	glCompileShader(fs);
+	//link the shaders into a single program
+	glAttachShader(retval.handle, vs);
+	glAttachShader(retval.handle, fs);
+	glLinkProgram(retval.handle);
+	//No longer need these. Their functionality has been eaten by the program
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+	return retval;
+}
+
+void freeShader(Shader &)
+{
+
 }
--- RenderUtils/crenderutils.h
+++ RenderUtils/crenderutils.h
@@ -10,7 +10,16 @@ struct Geometry
 	
 };
 
-Geometry makeGeometry(struct Vertex *verts, size_t vsize, 
-					  unsigned int	*tris,	size_t tsize);
+Geometry makeGeometry(const struct Vertex *verts, size_t vsize, 
+					  const unsigned int	*tris,	size_t tsize);
 
 void freeGeometry(Geometry &);
+
+struct Shader
+{
+	unsigned handle;
+};
+
+Shader makeShader(const char *vsource, const char *fsource);
+
+void freeShader(Shader &);

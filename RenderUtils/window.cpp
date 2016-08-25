cd52dbc263d62e8a5bff20bf9e62c26545e3b6d5
GilyeatArthur <gilyeatarthur@hotmail.com>
2016-08-25 12:23:02 -0700

    Now to make triangles

--- RenderUtils/window.cpp
+++ RenderUtils/window.cpp
@@ -19,7 +19,7 @@ bool Window::init(int a_width, int a_height, char *a_title)
 
 	glewExperimental = true;
 	glewInit();
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClearColor(1.0f, 0.0f, 1.0f, 1.0f);
 	return true;
 }
 

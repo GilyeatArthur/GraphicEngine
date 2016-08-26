32d04d23a89d554dc6163621f65dca784bdfadcb
GilyeatArthur <gilyeatarthur@hotmail.com>
2016-08-25 17:08:22 -0700

    Can Change colors

--- TriangleTest/main.cpp
+++ TriangleTest/main.cpp
@@ -8,19 +8,22 @@ int main()
 	Window window;
 	window.init(800, 600, "bleh");
 
-	Vertex	 vert[3] = { {   0,  .5f, 0, 1},
-						 { .5f, -.5f, 0, 1},
-						 {-.5f, -.5f, 0, 1} };
+	Vertex	 vert[3] = { {   0,  .5f, 0, 1, 1,0,0,0},
+						 { .5f, -.5f, 0, 1, 0,1,0,0},
+						 {-.5f, -.5f, 0, 1, 0,0,1,0} };
 	unsigned tris[3] = {0, 1, 2};
 
-	const char vsource[] = 
-		"#version 150\n"
-		"in vec4 position;"
-		"void main() { gl_Position = position; }";
+	const char vsource[] =
+		"#version 330\n"
+		"layout(location=0)in vec4 position;"
+		"layout(location=1)in vec4 color;"
+		"out vec4 vColor;"
+		"void main() { vColor = color; gl_Position = position; }";
 	const char fsource[] =
 		"#version 150\n"
+		"in vec4 vColor;"
 		"out vec4 outColor;"
-		"void main () { outColor = vec4(1.0, 0.0, 0.0, 1.0); }";
+		"void main () { outColor = vec4(0,0,0,0) + vColor; }";
 
 	Geometry geometry = makeGeometry(vert, 3, tris, 3);
 	Shader shader = makeShader(vsource, fsource);

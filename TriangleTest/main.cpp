4898823a87f17351ec95840a3435730d11ee70d2
GilyeatArthur <gilyeatarthur@hotmail.com>
2016-08-26 17:56:20 -0700

    Did some stuff

--- TriangleTest/main.cpp
+++ TriangleTest/main.cpp
@@ -13,6 +13,18 @@ int main()
 	window.init(800, 800, "bleh");
 	gallery.init();
 
+	Vertex	 vert[4] = { { 1, 1, 1, 1, 1,0,0,1 },
+						 { 1, 1, 0, 1, 1,0,0,1 },
+						 { 1, 0, 1, 1, 1,0,0,1 },
+						 { 0, 1, 1, 1, 1,0,0,1 } };
+	
+	unsigned tris[6] = { 0, 1, 2, 0, 1, 3 };
+
+	Geometry geometry1 = makeGeometry(vert, 4, tris, 4);
+
+	Shader shader1 = makeShader("../res/shaders/simpleVert.txt",
+								"../res/shaders/simpleFrag.txt");
+
 	gallery.loadShader("FUN", "../res/shaders/simpleVert.txt",
 							  "../res/shaders/simpleFrag.txt");
 
@@ -23,8 +35,17 @@ int main()
 	while (window.step())
 	{
 		time += 0.016667f;
-		draw(gallery.getShader("FUN"), gallery.getObject("SPHERE"), time);
+		//draw(gallery.getShader("FUN"), gallery.getObject("SPHERE"), time);
 	}
+
+	while (window.step())
+	{
+		//time += 0.016667f;
+		draw(shader1, geometry1, 1);
+	}
+
+	freeGeometry(geometry1);
+	freeShader(shader1);
 
 	gallery.term();
 	window.term();

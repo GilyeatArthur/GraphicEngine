8ab235e517613940aa7febe0ed81a782af45c539
GilyeatArthur <gilyeatarthur@hotmail.com>
2016-08-31 15:18:12 -0700

    Got the "quad" working, but broke it while making it do stuff, so I had to copy Esme's code.

--- RenderUtils/window.cpp
+++ RenderUtils/window.cpp
@@ -36,7 +36,7 @@ bool Window::init(int a_width, int a_height, char *a_title)
 
 	glewExperimental = true;
 	glewInit();
-	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+	glClearColor(0.0f, 1.0f, 1.0f, 1.0f);
 
 #ifdef _DEBUG
 
--- TriangleTest/main.cpp
+++ TriangleTest/main.cpp
@@ -13,14 +13,14 @@ int main()
 	window.init(800, 800, "bleh");
 	gallery.init();
 
-	Vertex	 vert[4] = { { 1, 1, 1, 1, 1,0,0,1 },
-						 { 1, 1, 0, 1, 1,0,0,1 },
-						 { 1, 0, 1, 1, 1,0,0,1 },
-						 { 0, 1, 1, 1, 1,0,0,1 } };
+	Vertex	 vert[] = { {  1,  1, 0, 1 },
+						{  1, -1, 0, 1 },
+						{ -1, -1, 0, 1 },
+						{ -1,  1, 0, 1 } };
 	
-	unsigned tris[6] = { 0, 1, 2, 0, 1, 3 };
+	unsigned tris[] = { 0,1,2, 2,3,0 };
 
-	Geometry geometry1 = makeGeometry(vert, 4, tris, 4);
+	Geometry geometry1 = makeGeometry(vert, 3, tris, 3);
 
 	Shader shader1 = makeShader("../res/shaders/simpleVert.txt",
 								"../res/shaders/simpleFrag.txt");
@@ -28,24 +28,17 @@ int main()
 	gallery.loadShader("FUN", "../res/shaders/simpleVert.txt",
 							  "../res/shaders/simpleFrag.txt");
 
-	gallery.loadObjectOBJ("SPHERE", "../res/models/cube.obj");
-	
-	float time = 0;
+	gallery.loadObjectOBJ("SPHERE", "../res/models/sphere.obj");
 
+	gallery.makeObject("quad", vert, 4, tris, 6);
+
+
+	float time = 0;
 	while (window.step())
 	{
 		time += 0.016667f;
-		//draw(gallery.getShader("FUN"), gallery.getObject("SPHERE"), time);
+		draw(gallery.getShader("FUN"), gallery.getObject("quad"), time);
 	}
-
-	while (window.step())
-	{
-		//time += 0.016667f;
-		draw(shader1, geometry1, 1);
-	}
-
-	freeGeometry(geometry1);
-	freeShader(shader1);
 
 	gallery.term();
 	window.term();
